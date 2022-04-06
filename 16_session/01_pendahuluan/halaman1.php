36c935f487ca0376a8446163ac93c984a2e19e28
Jeremy Santoso <97815661+jeromedesantos12@users.noreply.github.com>
2022-04-06 09:12:26 +0700

    Add files via upload

---
+++ 16_session/01_pendahuluan/halaman1.php
@@ -0,0 +1,9 @@
+<?php 
+session_start();
+
+$_SESSION["nama"] = "Sandhika Galih";
+
+// session = informasi disimpan di server (dalam 1 sesi)
+// 1 sesi = ketika browser di close atau di restart
+
+ ?>
---
+++ 17_cookie-(remember-me)/01_pendahuluan/halaman1.php
@@ -0,0 +1,8 @@
+<?php 
+// membuat cookie
+setcookie('nama', 'Sandhika', time()+60);
+
+// cookie = informasi disimpan di browser (default: dalam 1 sesi) (bisa di manipulasi)
+// 1 sesi = ketika browser di close atau di restart
+
+ ?>
