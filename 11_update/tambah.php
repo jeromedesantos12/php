752371c08d6f09417c6407effba20328c60375f8
Jeremy Santoso <97815661+jeromedesantos12@users.noreply.github.com>
2022-04-06 09:09:58 +0700

    Add files via upload

---
+++ 11_update/tambah.php
@@ -0,0 +1,62 @@
+<?php 
+require 'functions.php';
+
+// cek apakah tombol submit sudah ditekan atau belum
+if(isset($_POST["submit"])) {
+	
+	// cek apakah data berhasil di tambahkan atau tidak
+	if(tambah($_POST) > 0) {
+		echo "
+			<script>
+				alert('data berhasil ditambahkan!');
+				document.location.href = 'index.php';
+			</script>
+		";
+	} else {  
+		echo "
+			<script>
+				alert('data gagal ditambahkan!');
+				document.location.href = 'index.php';
+			</script>
+		";
+	}
+
+}
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tambah data mahasiswa</title>
+</head>
+<body>
+	<h1>Tambah data mahasiswa</h1>
+	<form action="" method="post">
+		<ul>
+			<li>
+				<label for="nrp">NRP :  </label>
+				<input type="text" name="nrp" id="nrp" required>
+			</li>
+			<li>
+				<label for="nama">Nama :  </label>
+				<input type="text" name="nama" id="nama" required>
+			</li>
+			<li>
+				<label for="nama">Email :  </label>
+				<input type="text" name="email" id="email" required>
+			</li>
+			<li>
+				<label for="nama">Jurusan :  </label>
+				<input type="text" name="jurusan" id="jurusan" required>
+			</li>
+			<li>
+				<label for="nama">Gambar :  </label>
+				<input type="text" name="gambar" id="gambar" required>
+			</li>
+			<li>
+				<button type="submit" name="submit">Tambah Data!</button>
+			</li>
+		</ul>
+	</form>
+</body>
+</html>
